2824996c924ab09c2198a191fcbdefcb10f7d39e
Oussama Bof <jackmarrow06@gmail.com>
2023-03-16 19:13:49 +0100

    form validation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ allInputs.forEach((inputField) => {
   });
 });
 
-/*-------------- function --------------*/
+/*-------------- main function --------------*/
 
 function validateInputs(e) {
   allInputs.forEach(function (input) {
@@ -21,62 +21,61 @@ function validateInputs(e) {
       deleteErrorMessage(input);
       const message = `${input.placeholder} cannot be empty`;
       createErrorMessage(input, message);
-    } else if (input.value.length !== 0) {
+    } else {
       if (input.id === "firstName" || input.id === "lastName") {
         const regex = /[^a-zA-Z0-9]/;
-        hasWrongStructureName(regex, input, e);
+        checkFirstAndLastName(regex, input, e);
       }
       if (input.id === "password") {
         const regex = /[!-/:-@[-`{-~]/;
-        hasWrongStructurePassword(regex, input, e);
+        checkPassword(regex, input, e);
       }
       if (input.id === "email") {
         const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        hasWrongStructureEmail(regex, input, e);
+        checkEmail(regex, input, e);
       }
     }
   });
 }
 
-function hasWrongStructureName(regex, input, e) {
+/*------------- helper function ----------*/
+
+function checkFirstAndLastName(regex, input, e) {
   const content = input.value;
   if (regex.test(content)) {
-    e.preventDefault();
-    backToNormalState(input);
     const message = `${input.placeholder} cannot contain special character`;
-    createErrorMessage(input, message);
+    getError(input, message, e);
   } else backToNormalState(input);
 }
 
-function hasWrongStructurePassword(regex, input, e) {
+function checkPassword(regex, input, e) {
   const content = input.value;
   if (input.value.length < 6) {
-    e.preventDefault();
-    backToNormalState(input);
     const message = `Password cannot be less than 6 characters`;
-    createErrorMessage(input, message);
+    getError(input, message, e);
   } else if (!regex.test(content)) {
-    e.preventDefault();
     const message = `Password should contain at least one special character`;
-    createErrorMessage(input, message);
+    getError(input, message, e);
   } else backToNormalState(input);
 }
 
-function hasWrongStructureEmail(regex, input, e) {
+function checkEmail(regex, input, e) {
   const content = input.value;
   if (!regex.test(content)) {
-    e.preventDefault();
-    backToNormalState(input);
     const message = `Looks like this is not an email`;
-    createErrorMessage(input, message);
+    getError(input, message, e);
   } else backToNormalState(input);
+}
+
+function getError(input, errorMessage, e) {
+  e.preventDefault();
+  backToNormalState(input);
+  createErrorMessage(input, errorMessage);
 }
 
 function createErrorMessage(ele, errorMessage) {
-  //select the parent div of the input
   const errorDiv = ele.parentElement;
   errorDiv.dataset.error = "true";
-  //create the error div
   const div = document.createElement("div");
   div.classList.add("error");
   div.innerText = errorMessage;
